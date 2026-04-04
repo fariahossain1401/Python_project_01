cb05f2276651d8c1db83556f12e6cae805bf1f78
Faria Hossain Prionti <fariahossain1401@gmail.com>
2026-04-04 19:36:26 +0600

    first commit

--- main.py
+++ main.py
@@ -5,22 +5,19 @@ import os
 
 DATA_FILE = "data.json"
 
-# Load data
 def load_data():
     if not os.path.exists(DATA_FILE):
         return []
     with open(DATA_FILE, "r") as file:
         return json.load(file)
 
-# Save data
 def save_data(data):
     with open(DATA_FILE, "w") as file:
         json.dump(data, file, indent=4)
 
-# Add expense
 def add_expense():
     amount = amount_entry.get()
-    category = category_entry.get()
+    category = category_var.get()
     date = date_entry.get()
 
     if amount == "" or category == "" or date == "":
@@ -44,10 +41,8 @@ def add_expense():
     messagebox.showinfo("Success", "Expense added!")
 
     amount_entry.delete(0, tk.END)
-    category_entry.delete(0, tk.END)
     date_entry.delete(0, tk.END)
 
-# Show expenses
 def show_expenses():
     data = load_data()
     listbox.delete(0, tk.END)
@@ -56,33 +51,43 @@ def show_expenses():
         text = f"{item['date']} | {item['category']} | {item['amount']}"
         listbox.insert(tk.END, text)
 
-# Show total
 def show_total():
     data = load_data()
     total = sum(item["amount"] for item in data)
     messagebox.showinfo("Total Expense", f"Total = {total}")
 
-# UI
 root = tk.Tk()
 root.title("Expense Tracker")
+root.geometry("400x500")
 
-tk.Label(root, text="Amount").pack()
-amount_entry = tk.Entry(root)
-amount_entry.pack()
+frame_top = tk.Frame(root)
+frame_top.pack(pady=20)
 
-tk.Label(root, text="Category").pack()
-category_entry = tk.Entry(root)
-category_entry.pack()
+tk.Label(frame_top, text="Amount").pack()
+amount_entry = tk.Entry(frame_top)
+amount_entry.pack(pady=5)
 
-tk.Label(root, text="Date").pack()
-date_entry = tk.Entry(root)
-date_entry.pack()
+tk.Label(frame_top, text="Category").pack()
 
-tk.Button(root, text="Add Expense", command=add_expense).pack()
-tk.Button(root, text="Show Expenses", command=show_expenses).pack()
-tk.Button(root, text="Show Total", command=show_total).pack()
+category_var = tk.StringVar()
+category_var.set("Select Category")
 
-listbox = tk.Listbox(root, width=50)
+categories = ["Bills", "Electricity", "Outings", "Grocery", "Extra"]
+category_menu = tk.OptionMenu(frame_top, category_var, *categories)
+category_menu.pack(pady=5)
+
+tk.Label(frame_top, text="Date").pack()
+date_entry = tk.Entry(frame_top)
+date_entry.pack(pady=5)
+
+tk.Button(frame_top, text="Add Expense", command=add_expense).pack(pady=10)
+tk.Button(frame_top, text="Show Expenses", command=show_expenses).pack(pady=5)
+tk.Button(frame_top, text="Show Total", command=show_total).pack(pady=5)
+
+frame_bottom = tk.Frame(root)
+frame_bottom.pack(pady=30)
+
+listbox = tk.Listbox(frame_bottom, width=45)
 listbox.pack()
 
 root.mainloop()
